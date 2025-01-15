1024b95db550fd886bb0acf69bd755a58e7bbef0
Denny Arfansyah <denny.arfansyah@ist.id>
2025-01-15 13:39:19 +0700

    Initial Commit

---
+++ TDD/TDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDDApp.swift
+//  TDD
+//
+//  Created by Denny Arfansyah on 15/01/25.
+//
+
+import SwiftUI
+
+@main
+struct TDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
